084b64ce40fd0eca381181d26972f197624cd9a4
Philbert Nderitu Wanjiku <blaw202458624@mylife.mku.ac.ke>
2025-07-15 20:17:08 +0300

    Create firebase.js

---
+++ src/firebase.js
@@ -0,0 +1,23 @@
+// src/firebase.js
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
+import { getStorage } from "firebase/storage";
+
+// Your web app's Firebase configuration from Replit Secrets
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Export the Firebase services you'll use in your components
+export const db = getFirestore(app);
+export const auth = getAuth(app);
+export const storage = getStorage(app);
